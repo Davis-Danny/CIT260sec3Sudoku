4adcfd10abf02481231f18162be59d08e37cb391
Davis-Danny <ddude1282@gmail.com>
2014-10-02 21:34:31 -0600

    1st Paired Programming Assignment

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -5,19 +5,35 @@
  */
 package sudoku;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Danny Davis
  */
 public class Sudoku {
-    
-    //comment
 
-    /**
-     * @param args the command line arguments
-     */
+    String playerName;
+    String instructions = "Pick a square to enter a number from 1 – 9 that you believe goes in that spot."
+            + " Continue until the squares are full and there are not any duplicate numbers in the rows, columns, or"
+            + " 3x3 sub-grids. Once you have completed the grid, push the done button.";
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Sudoku myGame = new Sudoku();
+        myGame.getName();
+        myGame.getHelp();
+        bestTimes myTimes = new bestTimes();
+        myTimes.showTimes();
     }
-    
+
+    public void getName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        playerName = input.next();
+        
+    }
+    public void getHelp(){
+        System.out.println(instructions);
+    }
+
 }
---
+++ src/sudoku/bestTimes.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+/**
+ *
+ * @author Danny
+ */
+public class bestTimes {
+    String time1 = "Not yet taken!";
+    String time2 = "Not yet taken!";
+    String time3 = "Not yet taken!";
+    String time4 = "Not yet taken!";
+    String time5 = "Not yet taken!";
+    
+    public void showTimes(){
+        System.out.println(time1);
+        System.out.println(time2);
+        System.out.println(time3);
+        System.out.println(time4);
+        System.out.println(time5);
+    }
+}
