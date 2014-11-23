3950623f25c33a83b1e0f127b3715cf9dca883bc
Danny <Danny@Steve10>
2014-11-22 23:28:24 -0700

    -added temporary classes Date and Birthday to fulfill requirements.

---
+++ src/sudoku/Birthday.java
@@ -0,0 +1,27 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package sudoku;
+//
+///**
+// *
+// * @author Danny
+// */
+//public class Birthday extends Date{
+//    
+//    public Birthday(String month,int day){
+//        super(month,day,0);
+//    }
+//    
+//    @Override
+//    public void getOccasion(){
+//        System.out.println("This date is a  birthday!");
+//    }
+//    
+//    @Override
+//    public void displayDate(){
+//        System.out.println("This birthday is on: "+month+" "+day+".");
+//    }
+//}
---
+++ src/sudoku/Date.java
@@ -0,0 +1,29 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package sudoku;
+//
+///**
+// *
+// * @author Danny
+// */
+//public abstract class Date {
+//    
+//    String month = null;
+//    int day = 0;
+//    int year = 0;
+//    
+//    public abstract void getOccasion();
+//    
+//    public Date(String month, int day, int year){
+//        this.month = month;
+//        this.day = day;
+//        this.year = year;
+//    }
+//    
+//    public void displayDate(){
+//        System.out.println(month + " " + day + "," + year);
+//    }
+//}
